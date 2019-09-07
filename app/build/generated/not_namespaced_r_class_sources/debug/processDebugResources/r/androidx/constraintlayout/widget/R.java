bca144b0c9401d4cec5c7ae02deea34d176273ca
Isaias <isaiascesariodacunha@gmail.com>
2019-09-07 12:05:39 -0300

    update

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -85,8 +85,8 @@ public final class R {
         public static final int spread = 0x7f0800a0;
         public static final int spread_inside = 0x7f0800a1;
         public static final int start = 0x7f0800a6;
-        public static final int top = 0x7f0800bf;
-        public static final int wrap = 0x7f0800ce;
+        public static final int top = 0x7f0800c0;
+        public static final int wrap = 0x7f0800cf;
     }
     public static final class styleable {
         private styleable() {}
--- app/src/main/java/br/com/isaias/atv_pdm_02_gasolina/MainActivity.java
+++ app/src/main/java/br/com/isaias/atv_pdm_02_gasolina/MainActivity.java
@@ -16,26 +16,29 @@ public class MainActivity extends AppCompatActivity {
 
     private double precoGasolina;
     private double precoEtanol;
+    private TextView textprecoGasolina;
+    private TextView textprecoEtanol;
     private TextView textviewmelhorusar;
     private ImageView imgViewMelhorUsar;
-
-    private double valeAPena = 0.0;
-    private int imagen;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void setImagen(int imagen) {
-        this.imagen = imagen;
+        textviewmelhorusar = findViewById(R.id.textTrocaMelhorUsar);
+        textprecoGasolina = findViewById(R.id.precoGasolina);
+        textprecoEtanol = findViewById(R.id.precoEtanol);
+        imgViewMelhorUsar = findViewById(R.id.imageViewMelhorUsar);
+        SeekBar seekBarGasolina = findViewById(R.id.seekBarGasolina);
+        SeekBar seekBarEtanol = findViewById(R.id.seekBarEtanol);
+        seekBarGasolina.setOnSeekBarChangeListener(onSeekBarChangeListenerGasolina);
+        seekBarEtanol.setOnSeekBarChangeListener(onSeekBarChangeListenerEtanol);
     }
 
     private SeekBar.OnSeekBarChangeListener onSeekBarChangeListenerGasolina = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
             precoGasolina = progress / 100D; //utilizando um D ou d alteramos o valor para double
+            textprecoGasolina.setText(currencyFormat.format(precoGasolina));
         }
         @Override
         public void onStartTrackingTouch(SeekBar seekBar) {
@@ -43,19 +46,35 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) {
-
+            if(precoEtanol/precoGasolina > 0.7){
+                textviewmelhorusar.setText(R.string.textMelhorUsarGasolina);
+                imgViewMelhorUsar.setImageResource(R.drawable.imagengasolina);
+            }
+            else{
+                textviewmelhorusar.setText(R.string.textMelhorUsarEtanol);
+                imgViewMelhorUsar.setImageResource(R.drawable.imagen);
+            }
         }
     };
     private SeekBar.OnSeekBarChangeListener onSeekBarChangeListenerEtanol = new SeekBar.OnSeekBarChangeListener() {
         @Override
         public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
             precoEtanol = progress / 100D; //utilizando um D ou d alteramos o valor para double
+            textprecoEtanol.setText(currencyFormat.format(precoEtanol));
         }
         @Override
         public void onStartTrackingTouch(SeekBar seekBar) {
         }
         @Override
         public void onStopTrackingTouch(SeekBar seekBar) {
+            if(precoEtanol/precoGasolina > 0.7){
+                textviewmelhorusar.setText(R.string.textMelhorUsarGasolina);
+                imgViewMelhorUsar.setImageResource(R.drawable.imagengasolina);
+            }
+            else{
+                textviewmelhorusar.setText(R.string.textMelhorUsarEtanol);
+                imgViewMelhorUsar.setImageResource(R.drawable.imagen);
+            }
         }
     };
 }
